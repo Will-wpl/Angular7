c9115aa18701ad2f756618ff46b1051c4accbe34
Willwpl <249491644@qq.com>
2019-02-26 09:07:26 +0800

    格式整理

--- src/app/video/videomonitoring.component.ts
+++ src/app/video/videomonitoring.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AllService } from '../service/service';
-import { VideoObj,MainUrl } from '../util';
+import { VideoObj, MainUrl } from '../util';
 import * as $ from 'jquery';
 declare const WebVideoCtrl
 @Component({
@@ -10,123 +10,123 @@ declare const WebVideoCtrl
 })
 export class VideomonitoringComponent implements OnInit {
   token = sessionStorage.token ? sessionStorage.token : '';
-  master=false;zoneId='';vedioImgUrl='';
-  videoList=[];video=VideoObj;
+  master = false; zoneId = ''; vedioImgUrl = '';
+  videoList = []; video = VideoObj;
   constructor(
     private router: Router,
-    private getData: AllService,) { }
-  selectAll(event):void{
-    if(event.target.checked){
-      this.master=true;
-      this.videoList.map((item)=>{
-        item.checked=true;
+    private getData: AllService, ) { }
+  selectAll(event): void {
+    if (event.target.checked) {
+      this.master = true;
+      this.videoList.map((item) => {
+        item.checked = true;
       })
       this.showVideo();
-    }else{
-      this.master=false;
-      this.videoList.map((item)=>{
-        item.checked=false;
+    } else {
+      this.master = false;
+      this.videoList.map((item) => {
+        item.checked = false;
       })
       this.showVideo();
     }
     //console.log(this.master);
   }
-  selectChange(event,index):void{ 
-    if(event.target.checked){
-      this.videoList[index].checked=true;
+  selectChange(event, index): void {
+    if (event.target.checked) {
+      this.videoList[index].checked = true;
       this.showVideo();
-    }else{
-      this.videoList[index].checked=false;
+    } else {
+      this.videoList[index].checked = false;
       this.showVideo();
     }
   }
-  ngOnChanges():void{
-    
+  ngOnChanges(): void {
+
   }
-  showVideo(){
-      this.videoList.map((item,index)=>{
-        if(item.checked){
-            this.video.clickStopRealPlay(index);
-            this.video.clickStartRealPlay(undefined,index,item.chSn,item.serverIp,item.serverPort);
-        }else{
-            this.video.clickStopRealPlay(index);
-        }
-      })
+  showVideo() {
+    this.videoList.map((item, index) => {
+      if (item.checked) {
+        this.video.clickStopRealPlay(index);
+        this.video.clickStartRealPlay(undefined, index, item.chSn, item.serverIp, item.serverPort);
+      } else {
+        this.video.clickStopRealPlay(index);
+      }
+    })
   }
   ngOnInit(): void {
-      this.zoneId = sessionStorage.zoneId;
-      this.getCheckList(4);
-      this.getImage(this.zoneId);
-      $(function () {
-          // 检查插件是否已经安装过
-          let iRet = WebVideoCtrl.I_CheckPluginInstall();
-          if (-1 == iRet) {
-              alert("您还未安装过插件，双击开发包目录里的WebComponentsKit.exe安装！");
-              return;
+    this.zoneId = sessionStorage.zoneId;
+    this.getCheckList(4);
+    this.getImage(this.zoneId);
+    $(function () {
+      // 检查插件是否已经安装过
+      let iRet = WebVideoCtrl.I_CheckPluginInstall();
+      if (-1 == iRet) {
+        alert("您还未安装过插件，双击开发包目录里的WebComponentsKit.exe安装！");
+        return;
+      }
+      let w = $(".video_main").width();
+      // 初始化插件参数及插入插件
+      WebVideoCtrl.I_InitPlugin(w, 430, {
+        bWndFull: true,     //是否支持单窗口双击全屏，默认支持 true:支持 false:不支持
+        iPackageType: 2,    //2:PS 11:MP4
+        iWndowType: 3,
+        bNoPlugin: true,
+        cbSelWnd: function (xmlDoc) {
+          // this.video.g_iWndIndex = parseInt($(xmlDoc).find("SelectWnd").eq(0).text(), 10);
+          // let szInfo = "当前选择的窗口编号：" + this.video.g_iWndIndex;
+          // this.video.showCBInfo(szInfo);
+        },
+        cbDoubleClickWnd: function (iWndIndex, bFullScreen) {
+          let szInfo = "当前放大的窗口编号：" + iWndIndex;
+          if (!bFullScreen) {
+            szInfo = "当前还原的窗口编号：" + iWndIndex;
           }
-          let w= $(".video_main").width();
-          // 初始化插件参数及插入插件
-          WebVideoCtrl.I_InitPlugin(w, 430, {
-              bWndFull: true,     //是否支持单窗口双击全屏，默认支持 true:支持 false:不支持
-              iPackageType: 2,    //2:PS 11:MP4
-              iWndowType: 3,
-              bNoPlugin: true,
-              cbSelWnd: function (xmlDoc) {
-                  // this.video.g_iWndIndex = parseInt($(xmlDoc).find("SelectWnd").eq(0).text(), 10);
-                  // let szInfo = "当前选择的窗口编号：" + this.video.g_iWndIndex;
-                  // this.video.showCBInfo(szInfo);
-              },
-              cbDoubleClickWnd: function (iWndIndex, bFullScreen) {
-                  let szInfo = "当前放大的窗口编号：" + iWndIndex;
-                  if (!bFullScreen) {            
-                      szInfo = "当前还原的窗口编号：" + iWndIndex;
-                  }
-                  this.video.showCBInfo(szInfo);                             
-                  // 此处可以处理单窗口的码流切换
-                  /*if (bFullScreen) {
-                      clickStartRealPlay(1);
-                  } else {
-                      clickStartRealPlay(2);
-                  }*/
-              },
-              cbEvent: function (iEventType, iParam1, iParam2) {
-                  if (2 == iEventType) {// 回放正常结束
-                    this.video.showCBInfo("窗口" + iParam1 + "回放结束！");
-                  } else if (-1 == iEventType) {
-                    this.video.showCBInfo("设备" + iParam1 + "网络错误！");
-                  } else if (3001 == iEventType) {
-                    this.video.clickStopRecord(this.video.g_szRecordType, iParam1);
-                  }
-              },
-              cbRemoteConfig: function () {
-                this.video.showCBInfo("关闭远程配置库！");
-              },
-              cbInitPluginComplete: function () {
-                  $("#divPlugin").remove();
-                  $(".video_main").prepend("<div id='divPlugin' class='plugin'></div>");
-                  WebVideoCtrl.I_InsertOBJECTPlugin("divPlugin");
-                  // 检查插件是否最新
-                  if (-1 == WebVideoCtrl.I_CheckPluginVersion()) {
-                      alert("检测到新的插件版本，双击开发包目录里的WebComponentsKit.exe升级！");
-                      return;
-                  }
-              }
-          });
-          // 窗口事件绑定
-          $(window).resize(
-            function () {
-                var $Restart = $("#divPlugin object");
-                if ($Restart.length > 0) {
-                    //var oSize = this.video.getWindowSize();
-                    $Restart.attr('width',$(".video_main").width());
-                }
-            }
-        );    
-          // //初始化日期时间
-          // var szCurTime = this.video.dateFormat(new Date(), "yyyy-MM-dd");
-          // $("#starttime").val(szCurTime + " 00:00:00");
-          // $("#endtime").val(szCurTime + " 23:59:59");
+          this.video.showCBInfo(szInfo);
+          // 此处可以处理单窗口的码流切换
+          /*if (bFullScreen) {
+              clickStartRealPlay(1);
+          } else {
+              clickStartRealPlay(2);
+          }*/
+        },
+        cbEvent: function (iEventType, iParam1, iParam2) {
+          if (2 == iEventType) {// 回放正常结束
+            this.video.showCBInfo("窗口" + iParam1 + "回放结束！");
+          } else if (-1 == iEventType) {
+            this.video.showCBInfo("设备" + iParam1 + "网络错误！");
+          } else if (3001 == iEventType) {
+            this.video.clickStopRecord(this.video.g_szRecordType, iParam1);
+          }
+        },
+        cbRemoteConfig: function () {
+          this.video.showCBInfo("关闭远程配置库！");
+        },
+        cbInitPluginComplete: function () {
+          $("#divPlugin").remove();
+          $(".video_main").prepend("<div id='divPlugin' class='plugin'></div>");
+          WebVideoCtrl.I_InsertOBJECTPlugin("divPlugin");
+          // 检查插件是否最新
+          if (-1 == WebVideoCtrl.I_CheckPluginVersion()) {
+            alert("检测到新的插件版本，双击开发包目录里的WebComponentsKit.exe升级！");
+            return;
+          }
+        }
       });
+      // 窗口事件绑定
+      $(window).resize(
+        function () {
+          var $Restart = $("#divPlugin object");
+          if ($Restart.length > 0) {
+            //var oSize = this.video.getWindowSize();
+            $Restart.attr('width', $(".video_main").width());
+          }
+        }
+      );
+      // //初始化日期时间
+      // var szCurTime = this.video.dateFormat(new Date(), "yyyy-MM-dd");
+      // $("#starttime").val(szCurTime + " 00:00:00");
+      // $("#endtime").val(szCurTime + " 23:59:59");
+    });
   }
   getImage(id) {
     this.getData.findImg('imgC/findOne', this.token, 0, id, sessionStorage.menuId).then(result => {
@@ -137,27 +137,27 @@ export class VideomonitoringComponent implements OnInit {
       }
     })
   }
-  ngAfterViewInit(){
-      setTimeout(()=>{
-        if(this.videoList.length>0){
-          this.video.clickLogin(this.videoList)
-        }
-      },1000);
-      setTimeout(()=>{this.showVideo()},1500);
+  ngAfterViewInit() {
+    setTimeout(() => {
+      if (this.videoList.length > 0) {
+        this.video.clickLogin(this.videoList)
+      }
+    }, 1000);
+    setTimeout(() => { this.showVideo() }, 1500);
   }
-  show_video_control(type){
-    if(type=="open"){
-        $(".video_btn").hide();
-        $(".video_control_nr").fadeIn(300);
-    }else{
-        $(".video_btn").fadeIn(300);
-        $(".video_control_nr").hide();
+  show_video_control(type) {
+    if (type == "open") {
+      $(".video_btn").hide();
+      $(".video_control_nr").fadeIn(300);
+    } else {
+      $(".video_btn").fadeIn(300);
+      $(".video_control_nr").hide();
     }
   }
   getCheckList(limited) {
-    this.getData.getCameraZone('cameraC/getCameraInfos', this.token,this.zoneId).then(result => {
+    this.getData.getCameraZone('cameraC/getCameraInfos', this.token, this.zoneId).then(result => {
       const videoList = [];
-      if (result.beanModel.length>0) {
+      if (result.beanModel.length > 0) {
         result.beanModel.map((item, index) => {
           let vidoeObj = item;
           if (index < limited) {
